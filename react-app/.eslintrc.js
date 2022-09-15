68328f684495fe6aea7b3a730819376f8cf54546
Kouki Murakami <m.kouki.0928@gmail.com>
2022-09-15 07:14:39 +0000

    environmental construction

--- react-app/.eslintrc.js
+++ react-app/.eslintrc.js
@@ -8,7 +8,9 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
