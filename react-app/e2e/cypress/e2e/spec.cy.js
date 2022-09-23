5444a561489dbdc3fe5126c1ff655da809b243da
Kouki Murakami <m.kouki.0928@gmail.com>
2022-09-23 06:57:13 +0000

    add 課題2.1 上級編

--- react-app/e2e/cypress/e2e/spec.cy.js
+++ react-app/e2e/cypress/e2e/spec.cy.js
@@ -4,15 +4,18 @@ describe('empty spec', () => {
   });
 
   it('passes', () => {
-    cy.get('.game-info div').first().should('have.text', 'Next player: X');
+    cy.get('.game-info > div').first().should('have.text', 'Next player: X');
   });
 
   it('square', () => {
-    cy.get('.game-board div')
-      .find('div')
-      .find('button')
-      .first()
-      .click()
-      .should('have.text', 'X');
+    cy.get('.square').first().click().should('have.text', 'X');
+  });
+
+  it('square-line', () => {
+    cy.get('.square').as('squares');
+    for (let i = 0; i < 7; i++) {
+      cy.get('@squares').eq(i).click();
+    }
+    cy.get('.game-info > div').first().should('have.text', 'Winner: X');
   });
 });
