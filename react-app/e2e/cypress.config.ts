0ef7e081a9da16ff4d6629722eb1136e0ff1b2ea
Kouki Murakami <m.kouki.0928@gmail.com>
2022-09-24 06:18:09 +0000

    add 課題2.2

---
+++ react-app/e2e/cypress.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+module.exports = {
+  e2e: {
+    setupNodeEvents(
+      on: Cypress.PluginEvents,
+      config: Cypress.PluginConfigOptions
+    ) {
+      // implement node event listeners here
+    },
+  },
+};
--- react-app/e2e/cypress/e2e/spec.cy.ts
+++ react-app/e2e/cypress/e2e/spec.cy.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import cypress from 'cypress';
+
 describe('empty spec', () => {
   beforeEach(() => {
     cy.visit('/');
